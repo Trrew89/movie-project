99e9c02ba71b22893f6a776169ca54ce96860a00
Sveatoslav <sveatoslavbodrus@gmail.com>
2022-05-17 10:50:08 +0200

    if/else and switch

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,6 @@
 "use strict";
-
+/*
+//ПЕРВАЯ ПРОГРАММА
 const numberOfFilms = prompt('Сколько Фильмов вы посмотрели?', '');
 
 const personalMovieDB = {
@@ -18,3 +19,46 @@ const a = prompt ('Один из последних фильмов?', ' '),
 
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
+
+
+
+
+// ВТОРАЯ ПРОГРАММА
+if (4 == 9){
+    console.log("Ok!");
+
+} else {
+    console.log("not okay");
+}
+
+
+const num = 50;
+
+if(num < 49){
+    console.log("Error");
+
+} else if (num > 100){
+    console.log("Too much");
+
+} else {
+    console.log("Ok!");
+}
+
+(num === 50) ? console.log("Ok!") : console.log("Error!");
+*/
+
+const num = 50;
+switch (num){
+    case 49: 
+        console.log('Not true');
+        break;
+    case  100:
+        console.log('Not true');
+        break;
+    case 51:
+        console.log(' true');
+        break;
+    default:
+        console.log('Not this time');
+        break;
+}
